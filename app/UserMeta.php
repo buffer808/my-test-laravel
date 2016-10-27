bc733ef0e7dc3aa78114e49a43ff131012ca53bf
vagrant <vagrant@homestead>
2016-10-27 08:54:00 +0000

    first commit

---
+++ app/UserMeta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Phoenix\EloquentMeta\Meta;
+
+class UserMeta extends Meta
+{
+    protected $table = 'user_meta';
+}
---
+++ resources/views/vendor/adminlte/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	{{ trans('adminlte_lang::message.home') }}
+@endsection
+
+
+@section('main-content')
+	<div class="container-fluid spark-screen">
+		<h1>THIS IS A TEST FOR homestead:8000</h1>
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-heading">Home</div>
+
+					<div class="panel-body">
+						{{ trans('adminlte_lang::message.logged') }}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
